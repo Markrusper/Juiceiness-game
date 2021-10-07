3c162fae617a3d5a7805caae18f99b7cf6d6cb5a
lolgube010 <lolgube@gmail.com>
2021-10-07 12:27:25 +0200

    pushed everything from my old thing

---
+++ Assets/Scripts/Mohammeds saker (gammla)/ButtonStuff.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonStuff : MonoBehaviour
+{
+    // used for main menu, loads ur cutscene
+    public void Play() {
+        SceneManager.LoadScene(1);
+        Debug.Log("clicked play");
+    }
+
+    // simple quit button
+    public void Quit() {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    // skips our cutscene (which might not even exist tbh), loads our game
+    public void Skip() {
+        Debug.Log ("Skip Cutscene");
+        SceneManager.LoadScene(2);
+    }
+
+    // after we die
+    public void Restart(){
+        SceneManager.LoadScene(0);
+    }
+}
